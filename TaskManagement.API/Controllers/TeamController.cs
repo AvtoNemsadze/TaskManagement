4c5f61ac6ba91405051c5791a05ec0997a22a7a1
AvtandilNemsadze <a.nemsadze@lemondo.biz>
2024-01-23 17:20:31 +0400

    no message

--- TaskManagement.API/Controllers/TeamController.cs
+++ TaskManagement.API/Controllers/TeamController.cs
@@ -1,12 +1,11 @@
 ﻿using AutoMapper;
-using Azure.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading;
-using TaskManagement.Application.Responses;
-using TaskManagement.Application.Task.Queries.GetTaskDetails;
 using TaskManagement.Application.Team.Commands.CreateTeam;
+using TaskManagement.Application.Team.Commands.UpdateTeam.BlockTeamMember;
+using TaskManagement.Application.Team.Commands.UpdateTeam.RemoveUserFromTeam;
 using TaskManagement.Application.Team.Queries.GetTeamDetails;
+using TaskManagement.Application.Team.Queries.GetTeamMembersList;
 
 namespace TaskManagement.API.Controllers
 {
@@ -39,6 +38,54 @@ namespace TaskManagement.API.Controllers
         {
             var team = await _mediator.Send(new GetTeamDetailsQuery { Id = id });
             return Ok(team);
+        }
+
+        [HttpPut("remove-team-member")]
+        public async Task<IActionResult> RemoveTeamMember(RemoveTeamMemberModel model)
+        {
+            var command = new RemoveTeamMemberCommand
+            {
+                TeamId = model.TeamId,
+                UserIdToRemove = model.UserIdToRemove,
+                RequestingUserId = model.RequestingUserId
+            };
+
+            var response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpGet("{teamId}/user-ids")]
+        public async Task<IActionResult> GetUserIdsByTeamId(int teamId)
+        {
+            var query = new GetTeamMembersQuery { TeamId = teamId };
+            var userIds = await _mediator.Send(query);
+
+            return Ok(userIds);
+        }
+
+        [HttpPut("block-team-member")]
+        public async Task<IActionResult> BlockTeamMember(BlockTeamMemberModel requestModel)
+        {
+            var validator = new BlockTeamMemberModelValidator();
+            var validationResult = validator.Validate(requestModel);
+
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+
+            var command = new BlockTeamMemberCommand
+            {
+               TeamId = requestModel.TeamId,
+               UserIdToBlock = requestModel.UserIdToBlock,
+               RequestingUserId = requestModel.RequestingUserId,
+               Duration = requestModel.Duration
+            };
+
+            var response = await _mediator.Send(command);
+
+            return Ok(response);
         }
     }
 }
