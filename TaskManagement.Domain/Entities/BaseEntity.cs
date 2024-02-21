f72592ba2684678e9edecbff8a48b1d9dc40078f
AvtandilNemsadze <a.nemsadze@lemondo.biz>
2024-02-21 15:46:19 +0400

    add `abstract` modifier

--- TaskManagement.Domain/Entities/BaseEntity.cs
+++ TaskManagement.Domain/Entities/BaseEntity.cs
@@ -1,6 +1,6 @@
 ﻿namespace TaskManagement.Domain.Entities
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; } 
